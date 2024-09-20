af94794f25a1b0fd808c3541ad306878dbc807a4
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-09-20 20:45:08 +0300

    lessons 2-3

---
+++ app/models/question.rb
@@ -0,0 +1,10 @@
+class Question < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 5 }
+
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+end
+
+
