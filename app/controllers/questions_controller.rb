cfefbad0400817975173aea83014d2ca2d875034
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-09-21 22:46:46 +0300

    lesson4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,6 +10,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
+      flash[:success] = "Question successfully created!"
       redirect_to questions_path
     else
       render :new
@@ -27,6 +28,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find_by id: params[:id]
     if @question.update question_params
+      flash[:success] = "Question successfully updated"
       redirect_to questions_path
     else
       render :edit
@@ -36,6 +38,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find_by id: params[:id]
     @question.destroy
+    flash[:success] = "Question successfully deleted!"
     redirect_to questions_path
   end
 
