4b4a2f94183d325386ab643a63bab3d59b5de98a
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-09-25 14:33:20 +0300

    lesson5, error handling, one to many

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   def index
     @questions = Question.all
   end
@@ -18,15 +19,14 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find_by id: params[:id]
+    @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
   end
 
   def edit
-    @question = Question.find_by id: params[:id]
   end
 
   def update
-    @question = Question.find_by id: params[:id]
     if @question.update question_params
       flash[:success] = "Question successfully updated"
       redirect_to questions_path
@@ -36,15 +36,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by id: params[:id]
     @question.destroy
     flash[:success] = "Question successfully deleted!"
     redirect_to questions_path
   end
 
   private
-
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def set_question
+    @question = Question.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i[index new edit create update destroy show]
+  resources :questions do
+    resources :answers, only: [:create, :destroy]
+  end
 
   root "pages#index"
 end
