938e885fb562edf38fc43f86188a6178781481d3
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-09-26 23:43:49 +0300

    lesson 6, helpers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :set_question!
+  before_action :set_answer!, except: [:create]
 
   def create
     @answer = @question.answers.build(answer_params)
@@ -14,18 +15,15 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    answer = @question.answers.find params[:id]
-    answer.destroy
+    @answer.destroy
     flash[:success] = "Answer deleted!"
     redirect_to question_path(@question)
   end
 
   def edit
-    @answer = @question.answers.find params[:id]
   end
 
   def update
-    @answer = @question.answers.find params[:id]
     if @answer.update(answer_params)
       flash[:success] = "Answer updated!"
       redirect_to @question
@@ -42,5 +40,9 @@ class AnswersController < ApplicationController
 
   def set_question!
     @question = Question.find params[:question_id]
+  end
+
+  def set_answer!
+    @answer = @question.answers.find params[:id]
   end
 end
