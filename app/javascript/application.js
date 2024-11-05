66d9d0d191d731d6ed708cce8caec656e60ac53b
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-11-05 16:38:28 +0300

    lesson 16, select2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,14 @@
 // Entry point for the build script in your package.json
-// Entry point for the build script in your package.json
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
+import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
+import './scripts/select2'
 
 Rails.start()
 Turbolinks.start()
-
+ActiveStorage.start()
